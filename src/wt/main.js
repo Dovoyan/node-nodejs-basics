d8fe4ef5f2cbc3e03835a92c0d56689380627478
Dovoyan <70629154+Dovoyan@users.noreply.github.com>
2022-12-06 02:51:06 +0300

    add lost half

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from 'worker_threads'
+import os from 'os'
+
 const performCalculations = async () => {
-    // Write your code here
+    let cpuLen = (os.cpus().length);
+    let threads = [];
+
+    for (let i = 0; i < cpuLen; i++) {
+
+        const promise = new Promise((resolve, reject) => {
+            const worker = new Worker("./worker.js", { workerData: 10 + i });
+            worker.once('message', (message) => {
+                resolve(message)
+            });
+        });
+        threads.push(promise);
+    }
+    let res = await Promise.allSettled(threads)
+
+
+    let finalres = res.map((el) => {
+        let obj = {}
+        if (el.status === 'fulfilled') {
+            obj.status = 'resolved'
+            obj.data = el.value;
+        } else {
+            obj.status = 'error'
+            obj.data = null
+        }
+        return obj
+    })
+
+    console.log(finalres)
 };
 
 await performCalculations();
